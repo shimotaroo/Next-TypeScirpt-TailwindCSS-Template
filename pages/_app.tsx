07c4a6ab1e4e012bcb331513dbddcf26d1f11ebf
shimotaroo <shimotaro.0327@gmail.com>
2021-11-03 20:26:52 +0900

    add Head tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next.js+TypeScript+Tailwind CSS Template</title>
+        <meta name="description" content="Template of Next.js+TypeScript+Tailwind CSS" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
